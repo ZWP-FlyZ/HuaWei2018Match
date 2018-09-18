b1eb1195d0f1b7a1fd5762338494ee3c0ddc73c8
zwp <1258271806@qq.com>
2018-09-18 14:13:11 +0800

    更新多约束的分支界限法解决物理机装配问题

--- src/packing/packing1.py
+++ src/packing/packing1.py
@@ -12,7 +12,7 @@ Created on 2018年9月17日
 
 import math;
 import heapq;
-
+import copy;
 
 CPU_FLAG=0;
 MEM_FLAG=1;
@@ -29,10 +29,12 @@ class MaxHeapNode():
     up=0;
     lev=0;
     ptr=None;
-    def __init__(self,up,lev,ptr):
+    cw=None;
+    def __init__(self,up,lev,ptr,cw=None):
         self.up=up;
         self.lev=lev;
         self.ptr=ptr;
+        self.cw=cw;
     # 构造极大堆  改变符号
     def __gt__(self, other):
         return self.up < other.up
@@ -44,52 +46,77 @@ class MaxHeapNode():
         return self.up >= other.up
 
 
-def addToHeap(heap,bbnode,up,lchild,lev):
-    node = MaxHeapNode(up,lev,Bbnode(bbnode,lchild));
+def addToHeap(heap,bbnode,up,lchild,lev,cw=None):
+    node = MaxHeapNode(up,lev,Bbnode(bbnode,lchild),cw=cw);
     heapq.heappush(heap, node);
 
-def branch_bound(wc,wn,c):
-    n = len(wc);
+
+def branch_bound_mul(wc,wn,c,opt_tag):
+    '''
+    >多约束条件
+    wc.shape=[约束数目，类数量]
+    wn.shape=[类数量]
+    c.shape=[约束数目]
+    opt_tag:优化目标
+    '''
+    
+    # 约束条件个数
+    dim = len(wc);
+    
+    n = len(wc[0]);
+    
     r=[0]*n;
     if n>1:
         for i in range(n-2,0,-1):
-            r[i]=r[i+1]+wc[i+1]*wn[i+1];
-        r[0]=r[1]+wc[1]*wn[1];  
+            r[i]=r[i+1]+wc[opt_tag][i+1]*wn[i+1];
+        r[0]=r[1]+wc[opt_tag][1]*wn[1];  
               
     i=0;
-    cw = 0;
+    cw = [0]*dim;
     bbnode=None;# 空父节点
     bestw=0;
     bestx=[0]*n;
     heap=[];
+    
+    
+    # 判断各个约束条件是否满足
+    def is_ok(cw,wc,i,k,c):
+        for j in range(dim):
+            if cw[j]+wc[j][i]*k>c[j]:
+                return False;
+        return True;
+        
     while i<n:
         k = wn[i];
-        while k>0:
-            tmp = cw+wc[i]*k;
-            if tmp<=c:
+        while k>0:   
+            if is_ok(cw,wc,i,k,c):
                 # 添加左节点
+                # 主优化目标
+                tmp = cw[opt_tag]+wc[opt_tag][i]*k;
                 if tmp>bestw:
                     bestw=tmp;
                     besti=i;
                     bestk=k;
                     bestbb=bbnode;
-                tmp =tmp+r[i];    
-                addToHeap(heap,bbnode,tmp,k,i+1);
+                tmp = tmp+r[i];
+                # 更新cw
+                ncw = copy.deepcopy(cw);
+                for j in range(dim):
+                    ncw[j]=cw[j]+wc[j][i]*k;   
+                addToHeap(heap,bbnode,tmp,k,i+1,ncw);
             k-=1;
             
         # 添加右节点
-        if bestw<cw+r[i]:
-            addToHeap(heap,bbnode,cw+r[i],0,i+1);
+        if bestw<cw[opt_tag]+r[i]:
+            addToHeap(heap,bbnode,cw[opt_tag]+r[i],0,i+1,cw);
         
         if len(heap)==0:break;
         
         node =  heapq.heappop(heap);
         i = node.lev;
         bbnode = node.ptr;
-        cw = node.up - r[i-1];
+        cw = node.cw;
         
-        
-    cw = bestw;
     i=besti-1;
     bestx[besti]=bestk;  
     bbnode = bestbb;
@@ -98,7 +125,7 @@ def branch_bound(wc,wn,c):
         bbnode= bbnode.parent;
         i-=1;
             
-    return cw,bestx;
+    return bestw,bestx;
     
 
 
@@ -110,7 +137,7 @@ def packing(v_mac_names,pakcing_tab,p_mac,opt_tag='CPU'):
     '''
     v_cpu_ned = 0;v_mem_ned=0;
     opt_flag=CPU_FLAG;# 默认CPU 优化
-    opt_wc=[];opt_wn=[];opt_vname=[];
+    opt_wc=[[],[]];opt_wn=[];opt_vname=[];
     
     if opt_tag == 'MEM':opt_flag=MEM_FLAG;
     
@@ -118,7 +145,8 @@ def packing(v_mac_names,pakcing_tab,p_mac,opt_tag='CPU'):
         idx = -i-1;
         opt_vname.append(v_mac_names[idx]);
         la = pakcing_tab[idx];
-        opt_wc.append(la[opt_flag]);# 获得虚拟机优化目标列表
+        opt_wc[0].append(la[0]);# 获得虚拟机优化目标列表
+        opt_wc[1].append(la[1]);
         opt_wn.append(la[-1]);
         
         # 观察至少需要几台机器的统计
@@ -142,7 +170,9 @@ def packing(v_mac_names,pakcing_tab,p_mac,opt_tag='CPU'):
     
     
     def reoge(vn,vwc,vwn,mask):
-        nvn=[];nvwc=[];nvwn=[];
+        nvn=[];
+        nvwc=[[] for _ in range(len(vwc))];
+        nvwn=[];
         res=[]; 
         for i in range(len(mask)):
             if mask[i]>0:
@@ -150,44 +180,32 @@ def packing(v_mac_names,pakcing_tab,p_mac,opt_tag='CPU'):
                 res.append([vn[i],mask[i]]);
             if vwn[i]-mask[i]>0:
                 nvn.append(vn[i]);
-                nvwc.append(vwc[i]);
+                for j in range(len(vwc)):
+                    nvwc[j].append(vwc[j][i]);
                 nvwn.append(vwn[i]-mask[i]);
         return nvn,nvwc,nvwn,res;
-    
-    
+     
+     
     p_mac_cot=0;# 物理机数量统计
     packing_res=[];
-    while len(opt_wc)>0:
+    while len(opt_wn)>0:
         print('\nreap%d'%(p_mac_cot));
         print('opt_vname:\t',opt_vname);
         print('opt_wc  :\t',opt_wc);
         print('opt_wn  :\t',opt_wn); 
-        c,x = branch_bound(opt_wc,opt_wn,p_mac[opt_flag]);
+        c,x = branch_bound_mul(opt_wc,opt_wn,p_mac,opt_flag);
         print('res_used:\t',c); 
         print('mask    :\t',x); 
         opt_vname,opt_wc,opt_wn,rec = reoge(opt_vname,opt_wc,opt_wn,x);
         packing_res.append(rec);  
         p_mac_cot+=1;
-    
+     
     print(p_mac_cot);
     for i in packing_res:
         print(i);
     
+    return packing_res;
     
-#     for rep in range(min_pmac):
-#         print('reap%d'%(rep));
-#         print('opt_list:\t',opt_list);
-#         c,x=branch_bound(opt_list,p_mac[opt_flag]);
-#         print(c,x);
-#         tmp=[];
-#         for i in range(len(opt_list)):
-#             if x[i]==0:
-#                 tmp.append(opt_list[i]);
-#         opt_list=tmp;
-#         
-#     print(opt_list);
-    
-
 def run():
     
     pakcing_tab=[[1,1,15],
@@ -197,7 +215,7 @@ def run():
                  [16,16,15]];
     p_mac=[56,128];
     vnames=['fv1','fv4','fv8','fv11','fv13'];
-    packing(vnames,pakcing_tab,p_mac,'CPU');
+    packing(vnames,pakcing_tab,p_mac,'MEM');
     
     
     pass;
